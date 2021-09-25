9fe68f924080ed1654fedcdcecd1061c649c16f0
Nick Lindau <nicholaithegreat@gmail.com>
2021-09-24 21:17:01 -0700

    create basic api requests for frward geolocating and weather

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-console.log('Starting');
+const request = require('request');
 
-setTimeout(() => {
-    console.log('i found you miss new booty');
-}, 2000)
+const url = 'http://api.weatherstack.com/current?access_key=32169b2fd90037874dc5d0a0e97de9ad&query=37.8267,-122.4233&units=f'
 
-const timer = setInterval(() => {
-  console.log('get ready');
-}, 500);
+request({ url: url, json: true }, (error, response) => {
+  const data = response.body.current
+  console.log(`The weather is ${data.weather_descriptions[0].toLowerCase()}. It is currently ${data.temperature}. It feels like ${data.feelslike}`);
+});
 
-setTimeout(() => {
-  clearInterval(timer)
-}, 2000);
+const geocodingURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGluZGEwOTMiLCJhIjoiY2t0ejhwZTN0M2FzaTJvcDNnaTJ5enJ4eSJ9.nDI9-uHMJ14I0LjTJakqqg&limit=1'
 
-console.log('Stopping');
+request({ url: geocodingURL, json: true }, (error, response) => {
+  const data = response.body.features[0].center
+  const [long, lat] = data
+  console.log(`longitude: ${long}, latitude: ${lat}`);
+});
