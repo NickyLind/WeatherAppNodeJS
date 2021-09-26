71b34da48bbaf04a7b0f1a1ba9ce09d6b57d0fc7
Nick Lindau <nicholaithegreat@gmail.com>
2021-09-26 13:43:30 -0700

    add error handling for http requests

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=32169b2fd90037874dc5d0a0e97de9ad&query=37.8267,-122.4233&units=f'
+// const url = 'http://api.weatherstack.com/current?access_key=32169b2fd90037874dc5d0a0e97de9ad&query=37.8267,-122.4233&units=f'
 
-request({ url: url, json: true }, (error, response) => {
-  const data = response.body.current
-  console.log(`The weather is ${data.weather_descriptions[0].toLowerCase()}. It is currently ${data.temperature}. It feels like ${data.feelslike}`);
-});
+// request({ url: url, json: true }, (error, response) => {
+//   if (error) {
+//     console.log(`An error has occured: ${error.code}`);
+//   } else if (response.body.success === false) {
+//     console.log(`There was an error - code: ${response.body.error.code} \n message: ${response.body.error.type} - ${response.body.error.info}`);
+//   } else {
+//     const data = response.body.current
+//     console.log(`The weather is ${data.weather_descriptions[0].toLowerCase()}. It is currently ${data.temperature}. It feels like ${data.feelslike}`);
+//   }
+// });
 
-const geocodingURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGluZGEwOTMiLCJhIjoiY2t0ejhwZTN0M2FzaTJvcDNnaTJ5enJ4eSJ9.nDI9-uHMJ14I0LjTJakqqg&limit=1'
+const geocodingURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/ASSFUCK.json?access_token=pk.eyJ1IjoibGluZGEwOTMiLCJhIjoiY2t0ejhwZTN0M2FzaTJvcDNnaTJ5enJ4eSJ9.nDI9-uHMJ14I0LjTJakqqg&limit=1'
 
 request({ url: geocodingURL, json: true }, (error, response) => {
-  const data = response.body.features[0].center
-  const [long, lat] = data
-  console.log(`longitude: ${long}, latitude: ${lat}`);
+  if(error) {
+    console.log(`There was an error: ${error}`);
+  } else if (response.body.features.length === 0) {
+      console.log(`There was an error - unable to find location`);
+  } else {
+    const data = response.body.features[0].center
+    const [long, lat] = data
+    console.log(`longitude: ${long}, latitude: ${lat}`);
+  }
 });
