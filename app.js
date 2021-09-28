ce0f3d1681b03780f6b39c0dcf0809fcb403e289
Nick Lindau <nicholaithegreat@gmail.com>
2021-09-28 14:26:42 -0700

    refactor project to include destructuring and es6 object shorthand

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-console.log(process.argv);
+// console.log(process.argv);
 
 const address = process.argv[2];
 
@@ -10,17 +10,15 @@ if(!address) {
   return console.log('Please provide a correct address');
 }
 
-geocode(address, (error, data) => {
+geocode(address, (error, { latitude, longitude, location } = {}) => {
 
-  if(error) return console.log(error)
+  if(error) return console.log(`Error: ${error}`)
 
-  console.log('Error' , error);
-  console.log('Data: ', data);
-  forecast(data.latitude, data.longitude, (error, forecastData) => {
+  forecast(latitude, longitude, (error, forecastData) => {
 
     if(error) return console.log(error);
 
-    console.log(data.location);
+    console.log(location);
     console.log(forecastData);
   });
 });
--- utils/forecast.js
+++ utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request');
 const forecast = (lat, long, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=32169b2fd90037874dc5d0a0e97de9ad&query=${lat},${long}&units=f`
 
-  request( { url: url, json: true}, (error, response) => {
+  request( { url, json: true}, (error, { body }) => {
     if(error) {
       callback(`An error has occured: ${error.code}`)
-    } else if (response.body.success === false) {
-      callback(`There was an error \n code: ${response.body.error.info}`)
+    } else if (body.success === false) {
+      callback(`There was an error \n code: ${body.error.info}`)
     } else {
-      const data = response.body.current
+      const data = body.current
       callback(undefined, {
         data: `The weather is ${data.weather_descriptions[0].toLowerCase()}. It is currently ${data.temperature}. It feels like ${data.feelslike}`
       });
